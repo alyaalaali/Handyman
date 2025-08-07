756ce70a6f3532a65240e61c89fe71ac7135efa9
abdkhalid24 <busaud973@gmail.com>
2025-08-07 13:35:49 +0300

    Apply the provider selection for WorkerType.jsx

--- src/components/WorkerType.jsx
+++ src/components/WorkerType.jsx
@@ -9,6 +9,17 @@ const WorkerType = () => {
             <br />
             <button className="selection-button" type="submit">User</button>
         </form>
+        <br />
+
+        <form className="provider-selection" >
+            <h2>Provider Selection</h2>
+            <li>first definition</li>
+            <li>Second definition</li>
+            <li>Third definition</li>
+            <br />
+            <button className="selection-button" type="submit">Provider</button>
+        </form>
+
         
         </div>
     )
