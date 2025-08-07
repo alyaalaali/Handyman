baeb4bc6efc0103d9f02b59c455a88f4224acb31
abdkhalid24 <busaud973@gmail.com>
2025-08-07 15:22:51 +0300

    I've completed the form of ProSignUp.jsx

--- src/components/ProSignUp.jsx
+++ src/components/ProSignUp.jsx
@@ -22,6 +22,30 @@ const ProSignUp = () => {
             <input type="text" placeholder="Confirm your password" />
             <br />
 
+            <label for="fruit">Cetogery type: </label>
+
+            <select list="Cetogery" name="cetogery">
+            <option value=""></option>
+            <option value="Fitness">Fitness</option>
+            <option value="Carpentry">Carpentry</option>
+            <option value="Mechanical">Mechanical</option>
+            <option value="Education">Education</option>
+            <option value="Medicine">Medicine</option>
+            <option value="Engineering">Engineering</option>
+            <option value="Marketing">Marketing</option>   
+            <option value="Information Technology">IT</option>  
+            <option value="Human Resources">Human Resources</option> 
+            <option value="Media & Communications">Media & Communications</option>  
+            <option value="Artificial Intelligence">AI</option>  
+            <option value="Legal">Legal</option>  
+            <option value="Healthcare">Healthcare</option>   
+            <option value="Manufacturing">Manufacturing</option>   
+            <option value="Agriculture">Agriculture</option>  
+            <option value="Security & Law Enforcement">Security & Law Enforcement</option>
+            </select>
+
+            <br />
+            <br />
             <button type="submit">continue</button>
             
         </form>
