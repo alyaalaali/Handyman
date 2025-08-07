1c3ad63c1d17329522bede10ce5fc002602f112b
abdkhalid24 <busaud973@gmail.com>
2025-08-07 13:35:08 +0300

    Apply the user selection for WorkerType.jsx

--- src/components/WorkerType.jsx
+++ src/components/WorkerType.jsx
@@ -1,3 +1,17 @@
 const WorkerType = () => {
+    return (
+        <div className="worker-type">
+        <form className="user-selection">
+            <h2>User Selection</h2>
+            <li>first definition</li>
+            <li>Second definition</li>
+            <li>Third definition</li>
+            <br />
+            <button className="selection-button" type="submit">User</button>
+        </form>
+        
+        </div>
+    )
+
 }
 export default WorkerType
