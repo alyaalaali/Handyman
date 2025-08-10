f56e3f5bacdf6126f3486f4876a5a2cbe0e013e9
alyaalaali <alya.alaali05@gmail.com>
2025-08-10 23:18:17 +0300

    rendered the active requests

--- src/components/userView/ActiveRequests.jsx
+++ src/components/userView/ActiveRequests.jsx
@@ -1,8 +1,38 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { BASE_URL } from "../../services/api"
+import { Link } from "react-router-dom"
+
 const ActiveRequests = () => {
+  const [activeRequests, setActiveRequests] = useState([])
+
+  useEffect(() => {
+    const getActiveRequsts = async () => {
+      const response = await axios.get(`${BASE_URL}/request/active`)
+      setActiveRequests(response.data)
+    }
+    getActiveRequsts()
+  }, [])
+
   return (
-    <>
-      <p>active requests</p>
-    </>
+    <div className="active-requests">
+      <h2>Active Requests</h2>
+      {activeRequests.length === 0 ? (
+        <p>No active requests found</p>
+      ) : (
+        <ul className="requestlist">
+          {activeRequests.map((request) => (
+            <Link to={`/requests/${request._id}`} className="request-link">
+              <li key={request._id} className="request-item">
+                <h3>{request.title}</h3>
+                <p>Description: {request.description}</p>
+                <p>{new Date(request.createdAt).toLocaleDateString()}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
 export default ActiveRequests
