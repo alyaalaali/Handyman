b3009c7639dd5b741ac9ffeec30b51cf91ba181b
M7139 <mohamedaljoamee@gmail.com>
2025-08-15 20:54:03 +0300

    deploy2

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://handyman-874fc6affb24.herokuapp.com'
+export const BASE_URL = 'https://handyman-874fc6affb24.herokuapp.com/'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
